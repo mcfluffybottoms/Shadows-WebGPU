96ad5862e140348960f787bf918a2c0a22d1375a
Алтунина Екатерина <166242132+mcfluffybottoms@users.noreply.github.com>
2025-11-18 18:16:10 +0000

    add grid

--- scenes/shadowmap/main.ts
+++ scenes/shadowmap/main.ts
@@ -1,8 +1,15 @@
-import * as THREE from 'three/webgpu'
-import { getRenderer, loadAndAddObject, loadMeshFromLink } from '../../src/renderer';
-import { addCamera, setupControls } from '../../src/camera';
+import * as THREE from "three/webgpu";
+import {
+  getRenderer,
+  loadAndAddObject,
+  loadMeshFromLink,
+} from "../../src/renderer";
+import { addCamera, setupControls } from "../../src/camera";
 
 const scene = new THREE.Scene();
+scene;
+const grid = new THREE.GridHelper(1000, 1000, 0x0c0c0c0, 0xdcdcdc);
+scene.add(grid);
 
 const camera = addCamera();
 camera.position.set(21.6, 4.3, -18);
@@ -12,20 +19,19 @@ const renderer = getRenderer();
 
 const obj = await loadAndAddObject("/itmo.obj", scene);
 if (obj) {
-    obj.scale.setScalar(0.0001);
-    scene.add(obj);
+  obj.scale.setScalar(0.0001);
+  scene.add(obj);
 } else {
-    console.error("not loaded");
+  console.error("not loaded");
 }
 
 const controls = setupControls(camera, renderer);
 
 function animate() {
-    controls.update();
-    console.log(camera.position);
-    console.log(camera.rotation);
-    renderer.render(scene, camera);
-
+  controls.update();
+  console.log(camera.position);
+  console.log(camera.rotation);
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
