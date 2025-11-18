dd273f74d3db02a71cbc9efa2f762c6b9a592ecd
Алтунина Екатерина <166242132+mcfluffybottoms@users.noreply.github.com>
2025-11-18 20:35:15 +0300

    navigation and mesh loading - basic

--- scenes/shadowmap/main.ts
+++ scenes/shadowmap/main.ts
@@ -1,23 +1,31 @@
 import * as THREE from 'three/webgpu'
-import { addObject, getRenderer } from '../../src/renderer';
+import { getRenderer, loadAndAddObject, loadMeshFromLink } from '../../src/renderer';
 import { addCamera, setupControls } from '../../src/camera';
 
 const scene = new THREE.Scene();
 
 const camera = addCamera();
+camera.position.set(21.6, 4.3, -18);
+camera.rotation.set(-2.6, 0.4, 2.9);
 
 const renderer = getRenderer();
 
-const obj = addObject();
-scene.add(obj);
+const obj = await loadAndAddObject("/itmo.obj", scene);
+if (obj) {
+    obj.scale.setScalar(0.0001);
+    scene.add(obj);
+} else {
+    console.error("not loaded");
+}
 
 const controls = setupControls(camera, renderer);
 
 function animate() {
-    obj.rotation.x += 0.01;
-    obj.rotation.y += 0.01;
     controls.update();
+    console.log(camera.position);
+    console.log(camera.rotation);
     renderer.render(scene, camera);
+
 }
 
 renderer.setAnimationLoop(animate);
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,6 @@
 import * as THREE from 'three/webgpu'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
 export function getRenderer(): THREE.WebGPURenderer {
     const renderer = new THREE.WebGPURenderer({ antialias: true });
@@ -7,9 +9,32 @@ export function getRenderer(): THREE.WebGPURenderer {
     return renderer
 }
 
-export function addObject(geometry?: THREE.BufferGeometry, material?: THREE.Material): THREE.Mesh {
-    const actualGeometry = geometry || new THREE.BoxGeometry(1, 1, 1);
-    const actualMaterial = material || new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const mesh = new THREE.Mesh(actualGeometry, actualMaterial);
-    return mesh
+export function loadMeshFromLink(path: string): Promise<THREE.Group> {
+    return new Promise((resolve, reject) => {
+        const loader = new OBJLoader();
+        loader.load(
+            path,
+            (root) => {
+                console.log("Children:", root.children);
+                root.traverse((child) => {
+                    if (child instanceof THREE.Mesh) {
+                        child.material = new THREE.MeshBasicMaterial({ color: 0xFFC0CB });
+                    }
+                });
+                resolve(root);
+            },
+            undefined,
+            reject
+        );
+    });
+}
+
+export async function loadAndAddObject(path: string, scene: THREE.Scene) {
+    try {
+        const loadedGroup = await loadMeshFromLink(path);
+        scene.add(loadedGroup);
+        return loadedGroup;
+    } catch (error) {
+        console.error("Failed to load model:", error);
+    }
 }
